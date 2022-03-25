1e9d0113ddad797edc16f57a792f72f3ef508a5d
Ali Alhusseini <ciwanbuhar@gmail.com>
2022-03-25 00:48:43 -0300

    add addChar function

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ function toggleShift() {
     shift = true;
 }
 function toggleCapsLock() {
-    capsLock = true;
+    capsLock = capsLock !== true;
 }
 function toggleDeleteKey() {
     deleteKey = true;
 }
 
+function addChar(selection) {
+    var currChars = $("#inputArea").val();
+    if (selection === "delete") {
+        $("#inputArea").val(currChars.substring(0, currChars.length - 1));
+    } else {
+        if (capsLock === true && isNaN(selection)) {
+            $("#words").val(currChars.concat(selection.toUpperCase()));
+        } else if (shift === true && isNaN(selection)) {
+            $("#inputArea").val(currChars.concat(selection.toUpperCase()));
+            shift = false;
+        } else {
+            $("#inputArea").val(currChars.concat(selection));
+        }
+    }
+}
