f6b339401e0958b69ba1aa523434960bc64fc5d3
Ali Alhusseini <ciwanbuhar@gmail.com>
2022-03-25 00:38:37 -0300

    created keyboard

---
+++ script.js
@@ -0,0 +1,14 @@
+let shift = false;
+let capsLock = false;
+let deleteKey = false;
+
+// toggle keys
+function toggleShift() {
+    shift = true;
+}
+function toggleCapsLock() {
+    capsLock = true;
+}
+function toggleDeleteKey() {
+    deleteKey = true;
+}
